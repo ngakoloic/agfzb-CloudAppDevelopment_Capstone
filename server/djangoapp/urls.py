481eb03863d5472887c973003a8bb07275f1eaa2
bngako <bngako@outlook.fr>
2024-01-01 19:21:07 -0500

    first dealer app

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
+    path(route='static/', view=views.static_page, name='static'),
 
     # path for about view
 

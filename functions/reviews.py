110678a1f0b70e8a81bc4cd3c5e3b4189794acfb
ngakoloic <142854147+ngakoloic@users.noreply.github.com>
2024-01-06 21:56:35 -0400

    week 4 final

--- functions/reviews.py
+++ functions/reviews.py
@@ -21,39 +21,47 @@ def get_reviews():
     dealership_id = request.args.get('dealerId')
 
     # Check if "id" parameter is missing
-    if dealership_id is None:
-        return jsonify({"error": "Missing 'id' parameter in the URL"}), 400
+    if (dealership_id == 'all'):
+        # return jsonify({"error": "Missing 'id' parameter in the URL"}), 400
+        # Execute the query using the query method
+        selector = {
+            'purchase': True
+            }
+        result = db.get_query_result(selector)
+        data_list = []
+        for doc in result:
+            data_list.append(doc)
+        return jsonify(data_list)
+    else :
+        # Convert the "id" parameter to an integer (assuming "id" should be an integer)
+        try:
+            dealership_id = int(dealership_id)
+        except ValueError:
+            return jsonify({"error": "'id' parameter must be an integer"}), 400
 
-    # Convert the "id" parameter to an integer (assuming "id" should be an integer)
-    try:
-        dealership_id = int(dealership_id)
-    except ValueError:
-        return jsonify({"error": "'id' parameter must be an integer"}), 400
+        # Define the query based on the 'dealership' ID
+        selector = {
+            'dealership': dealership_id
+        }
 
-    # Define the query based on the 'dealership' ID
-    selector = {
-        'dealership': dealership_id
-    }
+        # Execute the query using the query method
+        result = db.get_query_result(selector)
 
-    # Execute the query using the query method
-    result = db.get_query_result(selector)
+        # Create a list to store the documents
+        data_list = []
 
-    # Create a list to store the documents
-    data_list = []
+        # Iterate through the results and add documents to the list
+        for doc in result:
+            data_list.append(doc)
 
-    # Iterate through the results and add documents to the list
-    for doc in result:
-        data_list.append(doc)
-
-    # Return the data as JSON
-    return jsonify(data_list)
+        # Return the data as JSON
+        return jsonify(data_list)
 
 
 @app.route('/api/review', methods=['POST'])
 def post_review():
     if not request.json:
         abort(400, description='Invalid JSON data')
-    
     # Extract review data from the request JSON
     review_data = request.json
     # Get sub dic of dict
